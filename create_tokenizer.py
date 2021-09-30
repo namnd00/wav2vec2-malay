aae453ccae8c8ec64f464db53f5fb1e03949165f
unknown <kingkcode@gmail.com>
2021-09-30 10:23:26 +0700

    OK

--- create_tokenizer.py
+++ create_tokenizer.py
@@ -16,11 +16,13 @@ import json
 import time
 import datasets
 
-chars_to_ignore_regex = '[\,\?\.\!\-\;\:\\"\$]'
+chars_to_ignore_regex = ['"', "*", "()", "[\]", "`", "_", "+/=%|"]
+CHARS_TO_IGNORE = f'[{"".join(chars_to_ignore_regex)}]'
 
 
 def remove_special_characters(batch):
-    batch["transcript"] = re.sub(chars_to_ignore_regex, '', batch["transcript"]).lower()
+    batch["transcript"] = re.sub(CHARS_TO_IGNORE, '', batch["transcript"]).lower()
+    batch["transcript"] = re.sub("&", 'dan', batch["transcript"])
     return batch
 
 
