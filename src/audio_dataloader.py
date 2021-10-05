7b6ae59a61f54424f10ffe2db2f7e90a8a644dff
nam-nd-d3 <nam.nd.00@gmail.com>
2021-10-05 11:27:59 +0700

    fixed malaydatasets

--- src/audio_dataloader.py
+++ src/audio_dataloader.py
@@ -7,37 +7,35 @@ Time    : 9/24/2021 10:16 AM
 Desc:
 """
 import os
-
-os.environ["WANDB_DISABLED"] = "true"
 import logging
 import argparse
+import random
 from dataclasses import dataclass
-from pathlib import Path
 from time import time
 
-import datasets
 import numpy as np
 import pandas as pd
 import torch
 import torchaudio
-from datasets import load_metric, Dataset
-from torch.utils.data import DataLoader
+from datasets import load_metric
 from torchaudio_augmentations import (RandomApply,
                                       PolarityInversion,
                                       Noise,
                                       Gain,
                                       HighLowPass,
                                       Delay,
-                                      compose)
+                                      compose, PitchShift, Reverb)
 
-# from torchaudio_augmentations.augmentations.pitch_shift import PitchShift
-# from torchaudio_augmentations.augmentations.reverb import Reverb
-from typing import Any, Dict, List, Optional, Union
-from transformers import Wav2Vec2CTCTokenizer, Wav2Vec2FeatureExtractor, Wav2Vec2Processor, Wav2Vec2ForCTC, \
-    TrainingArguments, Trainer
-
-from preprocessing_dataset import remove_special_characters, speech_file_to_array_fn, prepare_dataset
-from src.dataloader import AudioDataLoader
+from typing import (Dict,
+                    List,
+                    Optional,
+                    Union)
+from transformers import (Wav2Vec2CTCTokenizer,
+                          Wav2Vec2FeatureExtractor,
+                          Wav2Vec2Processor,
+                          Wav2Vec2ForCTC,
+                          TrainingArguments,
+                          Trainer)
 
 logger = logging.getLogger(__name__)
 
@@ -63,29 +61,6 @@ class AudioProcessor:
 
 @dataclass
 class DataCollatorCTCWithPadding:
-    """
-    Data collator that will dynamically pad the inputs received.
-    Args:
-        processor (:class:`~transformers.Wav2Vec2Processor`)
-            The processor used for proccessing the data.
-        padding (:obj:`bool`, :obj:`str` or :class:`~transformers.tokenization_utils_base.PaddingStrategy`, `optional`, defaults to :obj:`True`):
-            Select a strategy to pad the returned sequences (according to the model's padding side and padding index)
-            among:
-            * :obj:`True` or :obj:`'longest'`: Pad to the longest sequence in the batch (or no padding if only a single
-              sequence if provided).
-            * :obj:`'max_length'`: Pad to a maximum length specified with the argument :obj:`max_length` or to the
-              maximum acceptable input length for the model if that argument is not provided.
-            * :obj:`False` or :obj:`'do_not_pad'` (default): No padding (i.e., can output a batch with sequences of
-              different lengths).
-        max_length (:obj:`int`, `optional`):
-            Maximum length of the ``input_values`` of the returned list and optionally padding length (see above).
-        max_length_labels (:obj:`int`, `optional`):
-            Maximum length of the ``labels`` returned list and optionally padding length (see above).
-        pad_to_multiple_of (:obj:`int`, `optional`):
-            If set will pad the sequence to a multiple of the provided value.
-            This is especially useful to enable the use of Tensor Cores on NVIDIA hardware with compute capability >=
-            7.5 (Volta).
-    """
 
     processor: Wav2Vec2Processor
     padding: Union[bool, str] = True
@@ -97,8 +72,7 @@ class DataCollatorCTCWithPadding:
     def __call__(
             self, features: List[Dict[str, Union[List[int], torch.Tensor]]]
     ) -> Dict[str, torch.Tensor]:
-        # split inputs and labels since they have to be of different lenghts and need
-        # different padding methods
+
         input_features = [
             {"input_values": feature["input_values"][0].squeeze()} for feature in features
         ]
@@ -139,8 +113,8 @@ class MalayAudioDataset:
             audio_dir,
             audio_processor,
             sample_rate=16_000,
-            num_augmented_samples=16,
-            have_transforms=True,
+            audio_transforms=None,
+            num_transforms=16,
             padding=True,
             device="cuda"
     ):
@@ -154,10 +128,10 @@ class MalayAudioDataset:
         self.audio_dir = audio_dir
         self.sample_rate = sample_rate
         self.device = device
-        self.have_transforms = have_transforms
-        if self.have_transforms:
-            self.transforms = self._get_audio_transforms()
-        self.num_augmented_samples = num_augmented_samples
+        self.audio_transforms = audio_transforms
+        if self.audio_transforms:
+            self._transforms = self._get_audio_transforms()
+        self.num_transforms = num_transforms
 
     def __len__(self):
         return len(self.annotations)
@@ -171,22 +145,16 @@ class MalayAudioDataset:
         transcript = self._get_audio_sample_transcript(index)
         signal, sr = torchaudio.load(audio_sample_path)
         signal = self._resample_if_necessary(signal, sr)
-        if self.have_transforms:
-            transform_many = compose.ComposeMany(transforms=self.transforms,
-                                                 num_augmented_samples=self.num_augmented_samples)
+        if self._transforms:
+            transform_many = compose.ComposeMany(transforms=self._transforms,
+                                                 num_augmented_samples=self.num_transforms)
             signal = transform_many(signal)
-        input_values, labels = self._prepare_dataset(signal, transcript)
-        # batch = [{'input_values': input_values, 'labels': labels}]
-        # print(index)
-        # # input_values = input_values.squeeze().numpy()
-        # # signal = signal.to(self.device)
-        # data_collator = self._get_data_collator()
-        # collate_batch = data_collator(batch)
+        input_values, labels = self._prepare_dataset(signal, sr, transcript)
+
         return {'input_values': input_values, "labels": labels}
 
     def _get_audio_sample_path(self, index):
         audio_file = f"{self.annotations.iloc[index, 0]}"
-        # print(index, audio_file)
         path = os.path.join(self.audio_dir, audio_file)
         return path
 
@@ -195,35 +163,32 @@ class MalayAudioDataset:
 
     def _resample_if_necessary(self, signal, sr):
         if sr != self.sample_rate:
-            resampler = torchaudio.transforms.Resample(sr, self.sample_rate)
-            signal = resampler(signal)
+            audio_resample = torchaudio.transforms.Resample(sr, self.sample_rate)
+            signal = audio_resample(signal)
         return signal
 
     def _get_audio_transforms(self):
-        num_samples = self.sample_rate * 5
+        num_samples = self.sample_rate * random.randint(1, 5)
         return [
-            # RandomResizedCrop(n_samples=num_samples),
             RandomApply([PolarityInversion()], p=0.8),
             RandomApply([Noise(min_snr=0.1, max_snr=0.5)], p=0.3),
             RandomApply([Gain()], p=0.3),
             HighLowPass(sample_rate=self.sample_rate),
-            # RandomApply([PitchShift(
-            #     n_samples=num_samples,
-            #     sample_rate=self.sample_rate
-            # )], p=0.4),
+            RandomApply([PitchShift(
+                n_samples=num_samples,
+                sample_rate=self.sample_rate
+            )], p=0.4),
             RandomApply([Delay(sample_rate=self.sample_rate)], p=0.5),
-            # RandomApply([Reverb(sample_rate=self.sample_rate)], p=0.3)
+            RandomApply([Reverb(sample_rate=self.sample_rate)], p=0.3)
         ]
 
-    def _prepare_dataset(self, speech_signal, transcript):
+    def _prepare_dataset(self, speech_signal, sr, transcript):
         # check that all files have the correct sampling rate
         assert (
-                self.sample_rate == 16_000
+                sr == 16_000
         ), f"Make sure all inputs have the same sampling rate of {self.audio_processor.feature_extractor.sampling_rate}."
 
         input_values = self.audio_processor(speech_signal, sampling_rate=self.sample_rate).input_values
-        # input_values = input_values[0].squeeze()
-        # input_values = torch.from_numpy(input_values)
         with self.audio_processor.as_target_processor():
             labels = self.audio_processor(transcript).input_ids
         return input_values, labels
@@ -241,40 +206,12 @@ class MalayAudioDataset:
         return self.audio_processor
 
 
-# def collate_fn(batch):
-#     # batched = [elem for elem in batch if elem is not None]
-#     # if all(batched) is None:
-#     #     return
-#     input_features, labels = list(zip(*batch))
-
-
-# def collate_fn(data):
-#     """
-#        data: is a list of tuples with (example, label, length)
-#              where 'example' is a tensor of arbitrary shape
-#              and label/length are scalars
-#     """
-#     labels, lengths = zip(**data)
-#     max_len = max(lengths)
-#     n_ftrs = data[0][0].size(1)
-#     features = torch.zeros((len(data), max_len, n_ftrs))
-#     labels = torch.tensor(labels)
-#     lengths = torch.tensor(lengths)
-#
-#     for i in range(len(data)):
-#         j, k = data[i][0].size(0), data[i][0].size(1)
-#         features[i] = torch.cat([data[i][0], torch.zeros((max_len - j, k))])
-#
-#     return features.float(), labels.long(), lengths.long()
-
-
 def parse_args():
     args = argparse.ArgumentParser(description="Data loader for audio")
     args.add_argument("--audio_path", type=str, required=True, help="Path to directory which contains audio")
     args.add_argument("--annotation_path", type=str, required=True, help="Path to csv annotation")
-    args.add_argument("--num_augmented_samples", type=int, default=16, required=True,
+    args.add_argument("--num_transform", type=int, default=16, required=True,
                       help="Number of augmented samples")
-    args.add_argument("--num_workers", type=int, default=1, required=True, help="Number of workers")
 
     return args.parse_args()
 
@@ -296,10 +233,12 @@ def demo():
 
     malay_dataset = MalayAudioDataset(annotation_file=ANNOTATION_PATH,
                                       audio_dir=AUDIO_PATH,
-                                      have_transforms=False,
-                                      audio_processor=audio_processor)
+                                      audio_transforms=True,
+                                      audio_processor=audio_processor,
+                                      num_transforms=16)
 
-    print(f"There are {len(malay_dataset)} samples in the datasets")
+    print(f"There are {len(malay_dataset)} samples in the datasets, "
+          f"data augmentation: {malay_dataset.num_transforms}")
 
     tokenizer = Wav2Vec2CTCTokenizer(vocab_path,
                                      unk_token="[UNK]",
@@ -373,5 +312,4 @@ def demo():
     print(f"{time_end - time_begin:02}")
     a = 1
 
-
-demo()
+# demo()
