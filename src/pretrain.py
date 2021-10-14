54eeebb5662403497413748ff2bf0df9757ea9a5
nam-nd-d3 <nam.nd.00@gmail.com>
2021-10-14 11:19:48 +0700

    update config and modified pretrain

--- src/pretrain.py
+++ src/pretrain.py
@@ -23,21 +23,36 @@ def main():
     parser.add_argument("--audio_path", default=None, type=str,
                         required=True, help="Path to unlabeled audio")
 
-    # parser.add_argument("--manifest_path", default=None, required=True,
-    #                     type=str, help="Path to English pretrain wav2vec model")
+    parser.add_argument("--learning_rate", default=0.005, required=True,
+                        type=float, help="Learning rate")
+                        
+    parser.add_argument("--wandb_project", default="self_supervised_wav2vec2_model", required=False,
+                        type=str, help="Weights and Biases project name to use for logging")                    
 
     parser.add_argument("--init_model", default=None, required=True,
                         type=str, help="Path to pretrain wav2vec model")
 
-    parser.add_argument("--batch_size", default=16, required=True,
-                        type=int, help="Batch size, try to decrease this number if any CUDA memory problems occur")
+    parser.add_argument("--max_tokens", default=480000, required=True,
+                        type=int, help="Max of tokens")
+                        
+    parser.add_argument("--max_epoch", default=100, required=True,
+                        type=int, help="Max of epoch")
 
     parser.add_argument("--config_dir", default=None, required=True,
                         type=str, help="Config dir for training")
 
     parser.add_argument("--config_name", default=None, required=True,
                         type=str, help="Config name for training")
+                        
+    parser.add_argument("--results_path", default="./results_path.json", required=False,
+                        type=str, help="Results path of eval")
 
+    parser.add_argument("--ext", default="wav", required=False,
+                        type=str, help="Extension of wav audio")
+                        
+    parser.add_argument("--valid_percent", default=0.05, required=False,
+                        type=int, help="Validation percent from entire dataset")
+                        
     args = parser.parse_args()
 
     # Prepare manifest file
@@ -47,7 +62,7 @@ def main():
     if not os.path.exists(temp_dir):
         os.makedirs(temp_dir)
 
-    cmd = 'python ' + MANIFEST_PATH + ' ' + args.audio_path + ' --dest ' + temp_dir + ' --ext wav --valid-percent 0.05'
+    cmd = 'python ' + MANIFEST_PATH + ' ' + args.audio_path + ' --dest ' + temp_dir + f' --ext {args.ext} --valid-percent {args.valid_percent}'
     print(cmd)
     print("Prepare training data manifest...")
     os.system(cmd)
@@ -73,11 +88,18 @@ def main():
         cmd.append("checkpoint.reset_dataloader=True")
         cmd.append("checkpoint.reset_meters=True")
 
-    # cmd.append("optimization.max_update=2000000")
-    cmd.append("dataset.num_workers=" + str(NUM_CPU))
-    cmd.append("dataset.max_tokens=" + str(args.batch_size))
+    cmd.append(f"common.wandb_project={args.wandb_project}")
+    cmd.append(f"common_eval.results_path={args.results_path}")
+    
+    cmd.append(f"optimization.lr=[{args.learning_rate}]")
+    cmd.append(f"optimization.max_epoch={args.max_epoch}")
+    
+    cmd.append(f"dataset.num_workers={NUM_CPU}")
+    cmd.append(f"dataset.max_tokens={args.max_tokens}")
+    
     cmd.append(f"--config-dir {args.config_dir}")
     cmd.append(f"--config-name {args.config_name}")
+    
     cmd = ' '.join(cmd)
     print("Training wav2vec 2.0 large model...")
     os.system(cmd)
