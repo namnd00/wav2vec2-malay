4ff3b24a5664f08c3c07b51f4d2c0a65edb6a988
lexuansanh <38468965+lexuansanh@users.noreply.github.com>
2021-10-23 18:39:58 +0700

    Update wav2vec2_predict_services.py

--- deploy_web/flask/predicts/wav2vec2_predict_services.py
+++ deploy_web/flask/predicts/wav2vec2_predict_services.py
@@ -1,60 +1,101 @@
-import malaya_speech
-#import numpy as np
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
+from ctcdecode import CTCBeamDecoder
+import soundfile as sf
+import yaml
+import torch
+import numpy as np
+import json
+import os
 
-SAVED_MODEL_PATH = "wav2vec2-conformer-large"
+# SAVED_MODEL_PATH = "wav2vec2-large-xlsr-53"
 MAX_LENGTH = 22050
 SAMPLE_RATE = 16000
 
-class _Wav2vec2_Predict_Services:
 
+class Wav2vec2PredictServices:
     model = None
+    processor = None
+    vocab = None
+    ctc_lm_params = None
+    kenlm_ctcdecoder = None
     _instance = None
 
     def predict(self, file_path):
         """
-        :param file_path (str): Path to audio file to predict
-        :return predicted_keyword (str): Keyword predicted by the model
+        :param file_path: Path to audio file to predict
+        :return predicted_keyword (str): Keyword predicted by the models
         """
 
         # extract MFCC
         signal = self.preprocess(file_path)
-
+        inputs = self.processor(signal, sampling_rate=16_000, return_tensors="pt", padding=True)
+        with torch.no_grad():
+            logits = self.model(inputs.input_values, attention_mask=inputs.attention_mask).logits
 
         # get the predicted label
-        predictions = self.model.predict([signal],decoder = 'beam', beam_size = 5)
-        print(predictions)
-        predicted_keyword = predictions[0]
-        return predicted_keyword
+        beam_results, beam_scores, timesteps, out_lens = self.kenlm_ctcdecoder.decode(logits)
+        pred_with_lm = "".join(self.vocab[n] for n in beam_results[0][0][:out_lens[0][0]])
+        predicts = pred_with_lm.strip()
 
+        print("pred_label: ", predicts)
+        # predictions = self.models.predict([signal],decoder = 'beam', beam_size = 5)
+        # print(predictions)
+        # predicted_keyword = predictions[0]
+        return predicts
 
-    def preprocess(self, file_path):
+    @staticmethod
+    def preprocess(file_path):
         """Extract MFCCs from audio file.
-        :param file_path (str): Path of audio file
+        :param file_path: (str): Path of audio file
+        :return signal: (np.array): array of speech signal
         """
 
         # load audio file
-        signal, sample_rate = malaya_speech.load(file_path)
+        signal, sample_rate = sf.read(file_path)
 
         return signal
 
 
-def Wav2vec2_Predict_Services():
+def init_services():
     """Factory function for Keyword_Spotting_Service class.
     :return _Keyword_Spotting_Service._instance (_Keyword_Spotting_Service):
     """
 
     # ensure an instance is created only the first time the factory function is called
-    if _Wav2vec2_Predict_Services._instance is None:
-        _Wav2vec2_Predict_Services._instance = _Wav2vec2_Predict_Services()
-        _Wav2vec2_Predict_Services.model = malaya_speech.stt.deep_ctc(model = SAVED_MODEL_PATH)
-    return _Wav2vec2_Predict_Services._instance
+    if Wav2vec2PredictServices._instance is None:
+        Wav2vec2PredictServices._instance = Wav2vec2PredictServices()
+        Wav2vec2PredictServices.model = Wav2Vec2ForCTC.from_pretrained(
+            "./models/w2v_xlsr_model/checkpoint-54075")
+        Wav2vec2PredictServices.processor = Wav2Vec2Processor.from_pretrained(
+            "./models/w2v_xlsr_model/checkpoint-54075")
+
+        Wav2vec2PredictServices.vocab = Wav2vec2PredictServices.processor.tokenizer.convert_ids_to_tokens(
+            range(0, Wav2vec2PredictServices.processor.tokenizer.vocab_size))
+        space_ix = Wav2vec2PredictServices.vocab.index('|')
+        Wav2vec2PredictServices.vocab[space_ix] = ' '
+        with open(os.path.join("./lm", "config_ctc.yaml"),
+                  'r') as config_file:
+            Wav2vec2PredictServices.ctc_lm_params = yaml.load(config_file, Loader=yaml.FullLoader)
+        Wav2vec2PredictServices.kenlm_ctcdecoder = CTCBeamDecoder(Wav2vec2PredictServices.vocab,
+                                                                  model_path="./lm/malay_lm.bin",
+                                                                  alpha=Wav2vec2PredictServices.ctc_lm_params[
+                                                                         'alpha'],
+                                                                  beta=Wav2vec2PredictServices.ctc_lm_params[
+                                                                         'beta'],
+                                                                  cutoff_top_n=40,
+                                                                  cutoff_prob=1.0,
+                                                                  beam_width=100,
+                                                                  num_processes=4,
+                                                                  blank_id=Wav2vec2PredictServices.processor.tokenizer.pad_token_id,
+                                                                  log_probs_input=True
+                                                                  )
+    return Wav2vec2PredictServices._instance
 
 
 if __name__ == "__main__":
-
     # create 2 instances of the keyword spotting service
-    wps = Wav2vec2_Predict_Services()
-    wps1 = Wav2vec2_Predict_Services()
+    wps = init_services()
+    wps1 = init_services()
 
     # check that different instances of the keyword spotting service point back to the same object (singleton)
     assert wps is wps1
